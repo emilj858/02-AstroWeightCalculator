82645e9a51530b80738a94373d4089af73b8652d
Emil Juboori <Emil@MacBook-Pro-2.local>
2016-11-10 14:41:18 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var planets = [
+  {planet: 'Sun', gravity: 27.9},
+  {planet: 'Mercury', gravity: 0.377},
+  {planet: 'Venus', gravity: 0.9032},
+  {planet: 'Earth', gravity: 1},
+  {planet: 'Moon', gravity: 0.1655},
+  {planet: 'Mars', gravity: 0.3895},
+  {planet: 'Jupiter', gravity: 2.640},
+  {planet: 'Saturn', gravity: 1.139},
+  {planet: 'Uranus', gravity: 0.917},
+  {planet: 'Neptune', gravity: 1.148},
+  {planet: 'Pluto', gravity: 0.06}
+];
+
+// generate the select element
+
+for(var i = 0; i < planets.length; i++) {
+  var planet = planets[i];
+
+  var optionElement = document.createElement('option');
+  optionElement.value = planet.gravity;
+  optionElement.innerHTML = planet.planet;
+
+
+var selectElement = document.getElementById('selectPlanet');
+selectElement.appendChild(optionElement);
+}
+
+// Bind a funciton to the click event
+
+var button = document.getElementById('calculateWeight');
+
+button.onclick = function (){
+  var inputWeight = document.getElementById('inputWeight');
+    if (isNaN(inputWeight.value)) {
+      alert ('Please input only numbers');
+    } 
+    else {
+      var weight = inputWeight.value;
+
+      var selectElement = document.getElementById('selectPlanet');
+
+      var selectedIndex = selectElement.selectedIndex;
+
+      var selectedOption = selectElement.options[selectedIndex];
+
+      var planetName = selectedOption.text;
+      var planetValue = selectedOption.value;
+
+      var userWeightOnPlanet = weight * planetValue;
+      userWeightOnPlanet = userWeightOnPlanet.toFixed(2);
+
+      var message = 'If you were on ' + planetName + ' , you would weigh ' + userWeightOnPlanet + 'lbs.';
+
+      var output = document.getElementById('output');
+}
+
+// Assign the message to the output element we just fetched from the DOM.
+  output.innerHTML = message; 
+};
+
+
+
+
+
+
+
+
+
+
+
